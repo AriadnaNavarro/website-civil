7bcf1734284820fbdb843063d41f8fa620b55a48
Jorge Vicente Chávez Vega <jorge.vicente2108@gmail.com>
2021-01-23 20:05:26 -0600

    primer commit

---
+++ carrousel.js
@@ -0,0 +1,23 @@
+const carrouselImages = document.querySelector('.carrousel_images');
+const carrouselButtons = document.querySelectorAll('.carrousel_button');
+const numberOfImages = document.querySelectorAll('.carrousel_images .image').length;
+
+let imageIndex = 1;
+let translateX = 0;
+
+carrouselButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        if (event.target.id === 'previous') {
+            if (imageIndex !== 1) {
+                imageIndex--;
+                translateX += 100;
+            }
+        } else {
+            if (imageIndex !== numberOfImages) {
+                imageIndex++;
+                translateX -= 100;
+            }
+        }
+        carrouselImages.style.left = `${ translateX }%`;
+    });
+});
