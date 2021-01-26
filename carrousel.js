346de487519200b45d1a75535c45b3088fd2e889
Jorge Vicente Chávez Vega <jorge.vicente2108@gmail.com>
2021-01-26 17:04:28 -0600

    configurado el carrusel con navbar

--- carrousel.js
+++ carrousel.js
@@ -2,6 +2,9 @@ const carrouselImages = document.querySelector('.carrousel_images');
 const carrouselButtons = document.querySelectorAll('.carrousel_button');
 const numberOfImages = document.querySelectorAll('.carrousel_images .image').length;
 
+const widthCarrousel = 100 * numberOfImages;
+carrouselImages.style.width = `${widthCarrousel}%`
+
 let imageIndex = 1;
 let translateX = 0;
 
