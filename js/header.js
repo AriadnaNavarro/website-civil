038a3e25b61eadb7ea1526b52e900e24c6369546
AriadnaNavarro <ariaddnavarro@gmail.com>
2021-10-18 17:43:32 -0500

    se modifica header

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 function abrirMenu() {
-    let menuu = document.getElementById('menuul');
+    let menuu = document.getElementById('menu');
     menuu.classList.toggle('abrirMenu')
 }
 
@@ -8,4 +8,20 @@ window.addEventListener('scroll', function() {
     let header = document.querySelector('header');
     let windowPosition = window.scrollY > 0;
     header.classList.toggle('scrolling-active', windowPosition);
+
+    let logo = document.getElementById('logoH');
+
+    if( windowPosition > 0 ){
+			logo.src= "assets/H-originalBlanco.png";
+		} else {
+			logo.src= "assets/H-original.png";
+
+		}
+
+    // let logo = document.getElementById('logoH').src = "assets/H-originalBlanco.png";
+    // logo.classList.
+
+
 });
+
+
